50610a44dfc0aeeb888f4845e4d641b43da58c2e
Mai Thành Duy An <tiachop0102@gmail.com>
2018-12-29 15:36:54 +0700

    Compare StringBuilder and "+=" Operator

--- HighPerformaceTest/VBHighPerfomaceWindowsApplication/Form1.vb
+++ HighPerformaceTest/VBHighPerfomaceWindowsApplication/Form1.vb
@@ -10,22 +10,22 @@ Public Class Form1
         Console.WriteLine("start")
         Dim sb As New StringBuilder()
         Dim startTime As DateTime = DateTime.Now
-        For i As Integer = 1 To 100000
+        For i As Integer = 1 To 10000
             sb.Append(i)
             sb.Append(", ")
         Next
         Dim endTime As DateTime = DateTime.Now
-        Console.WriteLine("Took: " & (endTime - startTime).ToString)
+        Console.WriteLine("String Builder Took: " & (endTime - startTime).ToString)
     End Sub
     Sub ConcateStringByPlus()
         Console.WriteLine("start")
         Dim sb As New StringBuilder()
         Dim str As String = ""
         Dim startTime As DateTime = DateTime.Now
-        For i As Integer = 1 To 100000
+        For i As Integer = 1 To 10000
             str += i.ToString
         Next
         Dim endTime As DateTime = DateTime.Now
-        Console.WriteLine("Took: " & (endTime - startTime).ToString)
+        Console.WriteLine("+= Took: " & (endTime - startTime).ToString)
     End Sub
 End Class
